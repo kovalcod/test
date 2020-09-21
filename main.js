e46e953e3d570b850cd3e1afd152fcf1d6d9d1af
kovalcod <44006407+kovalcod@users.noreply.github.com>
2020-09-21 21:19:37 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 $('.drop').click(function(){
+
     $('.drop-discr').toggle(500);
     $('.drop-title svg').toggleClass('revers');
+  
+});
+
+$(document).ready(function(){
+
+    $('.header_burger').click(function(event){
+        $('.header_burger, .header_menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
 
 });
