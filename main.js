ac1e842105959fc8cdfe70d95d7b7022ae2080c6
kovalcod <44006407+kovalcod@users.noreply.github.com>
2020-10-03 08:21:05 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-$(document).ready(function(){
-  $('.drop-title').click(function(){
-      if((this).classList.contains('active')){
-        $('.drop-discr').slideUp();
-        $(this).removeClass();
-      }else{
-        $('.drop-discr').slideDown();
-        $(this).addClass('active');
-      }
-    });
+var swiper = new Swiper('.swiper-container', {
+    spaceBetween:20,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    grabCursor: true,
+  });
+
+  $(document).ready(function(){
     $('.header_burger').click(function(event){
         $('.header_burger, .header_menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
-});
+  });
