324200d8a86d668b0ebd3659fca828c7c2b264b1
kovalcod <44006407+kovalcod@users.noreply.github.com>
2020-10-03 08:35:03 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,24 @@
+$('.drop').click(function() {
+    $('.drop-discr', this).toggle(500);
+    $('.drop-title svg', this).toggleClass('revers');
+
+});
+
 var swiper = new Swiper('.swiper-container', {
-    spaceBetween:20,
+    spaceBetween: 20,
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
     pagination: {
-      el: '.swiper-pagination'
+        el: '.swiper-pagination'
     },
     grabCursor: true,
-  });
+});
 
-  $(document).ready(function(){
-    $('.header_burger').click(function(event){
-        $('.header_burger, .header_menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
-  });
 
-$(document).ready(function(){
-  $('.drop-title').click(function(){
-      if((this).classList.contains('active')){
-        $('.drop-discr').slideUp();
-        $(this).removeClass();
-      }else{
-        $('.drop-discr').slideDown();
-        $(this).addClass('active');
-      }
-    });
-    $('.header_burger').click(function(event){
+$(document).ready(function() {
+    $('.header_burger').click(function(event) {
         $('.header_burger, .header_menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
