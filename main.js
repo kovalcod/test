afb22d19a28a586d3944fd64865d82fb564d5ce2
kovalcod <44006407+kovalcod@users.noreply.github.com>
2020-10-03 08:22:28 +0300

    Update main.js

--- main.js
+++ main.js
@@ -16,3 +16,19 @@ var swiper = new Swiper('.swiper-container', {
         $('body').toggleClass('lock');
     });
   });
+
+$(document).ready(function(){
+  $('.drop-title').click(function(){
+      if((this).classList.contains('active')){
+        $('.drop-discr').slideUp();
+        $(this).removeClass();
+      }else{
+        $('.drop-discr').slideDown();
+        $(this).addClass('active');
+      }
+    });
+    $('.header_burger').click(function(event){
+        $('.header_burger, .header_menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
