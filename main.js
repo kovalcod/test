9acefe9ac5b1dfa29c27d0e1f7dde7671f976505
kovalcod <44006407+kovalcod@users.noreply.github.com>
2020-10-03 07:53:46 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-var swiper = new Swiper('.swiper-container', {
-    spaceBetween:20,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-      el: '.swiper-pagination'
-    },
-    grabCursor: true,
-  });
-
-  $(document).ready(function(){
+$(document).ready(function(){
+  $('.drop-title').click(function(){
+      if((this).classList.contains('active')){
+        $('.drop-discr').slideUp();
+        $(this).removeClass();
+      }else{
+        $('.drop-discr').slideDown();
+        $(this).addClass('active');
+      }
+    });
     $('.header_burger').click(function(event){
         $('.header_burger, .header_menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
-  });
+});
